7576c08821f753ac30261091512f9a297320a789
Vaishnavi <153897977+Vaishnavishaligram@users.noreply.github.com>
2026-06-10 11:32:52 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar       from './components/Navbar'
+import Hero         from './components/Hero'
+import Tomorrow     from './components/Tomorrow'
+import WhatWeOffer  from './components/WhatWeOffer'
+import Testimonials from './components/Testimonials'
+import Newsletter   from './components/Newsletter'
+import Footer       from './components/Footer'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Tomorrow />
+      <WhatWeOffer />
+      <Testimonials />
+      <Newsletter />
+      <Footer />
+    </>
+  )
+}
